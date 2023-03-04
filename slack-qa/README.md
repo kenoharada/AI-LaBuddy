ec10070b72a71c1cea4c482edd19dcae7875a676
kenoharada <keno.0612.judo@icloud.com>
2023-03-05 01:30:05 +0900

    ready to active

--- slack-qa/README.md
+++ slack-qa/README.md
@@ -1,8 +1,22 @@
 # slack-qa
 
+https://slack.dev/bolt-python/ja-jp/tutorial/getting-started
+上記を参考に SLACK_BOT_TOKEN、SLACK_APP_TOKEN を発行
+
 ## 準備
 
 ```sh
-以下のコマンドで作られる.envファイルを編集しそれぞれTOKENを設定する
-cp .env{.example,}
+# 環境変数を設定して実行
+
+export SLACK_CHANNEL_ID=
+# Slack連携用
+export SLACK_BOT_TOKEN=xoxb-
+export SLACK_APP_TOKEN=xapp-
+# Open AIのAPI
+export OPENAI_API_KEY=sk-
+```
+
+```sh
+pip install slack_bolt openai tiktoken
+python3 app.py
 ```
--- slack-qa/app.py
+++ slack-qa/app.py
@@ -3,52 +3,102 @@ from slack_bolt import App
 from slack_sdk.errors import SlackApiError
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import openai
-
-CHANNEL_ID = os.environ.get("CHANNEL_ID")
-log_dir = os.path.join('log', CHANNEL_ID)
-if not os.path.exists(CHANNEL_ID):
+import tiktoken
+SLACK_CHANNEL_ID = os.environ["SLACK_CHANNEL_ID"]
+log_dir = os.path.join('log', SLACK_CHANNEL_ID)
+if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = os.environ["OPENAI_API_KEY"]
+# https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+encoding = tiktoken.get_encoding("cl100k_base")
+
 
 
 # スレッド内の全メッセージを取得する関数
 def get_thread_messages(channel_id, thread_ts):
-    messages = []
+    thread_messages = []
     try:
         result = app.client.conversations_replies(
             channel=channel_id,
-            ts=thread_ts
+            ts=thread_ts,
+            limit=1000
         )
-        messages = result["messages"]
+        thread_messages = result["messages"]
     except SlackApiError as e:
         print("Error getting thread messages: {}".format(e))
-    return messages
+    return thread_messages
 
 
-def create_response(messages):
+def create_gpt_message(thread_messages):
+    if len(thread_messages) == 1:
+        prompt = "あなたは人工知能・機械学習の研究をしている優秀な博士課程学生で、優秀なプログラマーです。技術的な質問や研究に関する質問に対しては分かりやすく丁寧に回答し、雑談のような問いかけには楽しく陽気に回答して下さい"
+        last_message = thread_messages[0]['text']
+        gpt_message = [
+                    {"role": "system", "content": prompt},
+                    {"role": "user", "content": last_message},
+                ]
+    elif len(thread_messages) == 0:
+        return ""
+    else:
+        # for thread_message in thread_messages:
+        history = ""
+        for thread_message in thread_messages:
+            if 'bot_id' in thread_message:
+                history += f"あなた: {thread_message['text']}\n"
+            else:
+                user_id = thread_message["user"]
+                user_name = app.client.users_info(user=user_id)["user"]["profile"]["display_name"]
+                history += f"{user_name}さん: {thread_message['text']}\n"
+                last_message = f"{user_name}さん: {thread_message['text']}\nあなた: "
+        if len(encoding.encode(history)) > 3072:
+            summary_prompt = f"以下は会話のやり取りです。やり取りを200文字程度に要約して下さい。\n\n{history}"
+            free_token_num = 4000 - len(encoding.encode(summary_prompt))
+            summarized_history = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages={"role": "user", "content": summary_prompt},
+                temperature=0,
+                max_tokens=free_token_num
+            )['choices'][0]['message']['content']
+            prompt =  f"以下は今までの会話の要約です。\n\n{summarized_history}\n\nあなたは人工知能・機械学習の研究をしている優秀な博士課程学生で、優秀なプログラマーです。技術的な質問や研究に関する質問に対しては分かりやすく丁寧に回答し、雑談のような問いかけには楽しく陽気に回答して下さい。"
+        else:
+            prompt =  f"以下は今までの会話です。\n\n{history}\n\nあなたは人工知能・機械学習の研究をしている優秀な博士課程学生で、優秀なプログラマーです。技術的な質問や研究に関する質問に対しては分かりやすく丁寧に回答し、雑談のような問いかけには楽しく陽気に回答して下さい。"
+        
+    gpt_message = [
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": last_message},
+        ]
+    with open(os.path.join(log_dir, f"{thread_messages[0]['thread_ts']}_{thread_messages[-1]['ts']}_prompt.txt"), 'w') as f:
+        f.write(prompt)
+    with open(os.path.join(log_dir, f"{thread_messages[0]['thread_ts']}_{thread_messages[-1]['ts']}_lastmessage.txt"), 'w') as f:
+        f.write(last_message)
+    
+    free_token_num = 4000 - (len(encoding.encode(prompt)) + len(encoding.encode(last_message)))
     response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=messages,
+                messages=gpt_message,
                 temperature=0,
-                max_tokens=4096
-            )
-    return response['choices'][0]['message']['content']
+                max_tokens=free_token_num
+            )['choices'][0]['message']['content']
+    with open(os.path.join(log_dir, f"{thread_messages[0]['thread_ts']}_{thread_messages[-1]['ts']}_response.txt"), 'w') as f:
+        f.write(response)
+    return response
 
 
 # ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 # メッセージを受信した時の処理
 @app.event("message")
 def handle_message(event, say):
-    query = event["text"]
     channel_id = event["channel"]
-    thread_ts = event["ts"]
-    print(channel_id)
-    if channel_id == os.environ.get("CHANNEL_ID"):
+    thread_ts = event["thread_ts"]
+
+    if channel_id == SLACK_CHANNEL_ID:
         thread_messages = get_thread_messages(channel_id, thread_ts)
-        response = create_response(query, thread_ts)
+        response = create_gpt_message(thread_messages)
+        print(thread_messages)
+        print(response)
         say(text=response, thread_ts=thread_ts)
     else:
         pass
